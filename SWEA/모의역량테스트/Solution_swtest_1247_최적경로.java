7df304cf32a353528b6ac651710d7e1522a38467
sonyeji <dpwl3385@gmail.com>
2020-02-28 19:25:31 +0900

    add swea 1247

---
+++ SWEA/모의역량테스트/Solution_swtest_1247_최적경로.java
@@ -0,0 +1,77 @@
+package swea0229;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution_swtest_1247_최적경로 {
+	static int N;
+	static int[] x;
+	static int[] y;
+	static int[][] map;
+	static int min;
+	static int[] list;
+	static boolean[] visit;
+	public static void main(String[] args) throws Exception, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine().trim());
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine().trim());
+			min = Integer.MAX_VALUE;
+			x = new int[2+N];
+			y = new int[2+N];
+			map = new int[2+N][2+N];
+			visit = new boolean[2+N];
+			list = new int[N];
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			x[0] = Integer.parseInt(st.nextToken());
+			y[0] = Integer.parseInt(st.nextToken());
+			x[1+N] = Integer.parseInt(st.nextToken());
+			y[1+N] = Integer.parseInt(st.nextToken());
+			
+			for (int i = 1; i < N+1; i++) {
+				x[i] = Integer.parseInt(st.nextToken());
+				y[i] = Integer.parseInt(st.nextToken());
+			}
+			makeMap();
+			nPr(0, 0);
+			System.out.println("#" + tc + " " + min);
+		}
+	}
+	
+	static void nPr(int flag, int count) {
+		if(count == N) {
+			int val = getValue();
+			min = Math.min(min, val);
+			return;
+		}
+		for (int i = 0; i < N; i++) {
+			if((flag&1<<i) == 0) {
+				list[count] = i+1;
+				nPr(flag | 1<<i, count+1);
+			}
+		}
+	}
+	
+	static void makeMap() {
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map.length; j++) {
+				if(i == j)	continue;
+				map[i][j] = Math.abs((x[i]-x[j])) + Math.abs((y[i]-y[j]));
+			}
+		}
+	}
+	
+	static int getValue() {
+		int val = map[0][list[0]];
+		for (int i = 0; i < list.length-1; i++) {
+			val += map[list[i]][list[i+1]];
+		}
+		val += map[list[N-1]][N+1];
+		return val;
+	}
+
+}
